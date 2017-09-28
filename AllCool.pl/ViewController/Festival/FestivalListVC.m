9ee60365fc05fc80fef00da3f2759bf20b0f070f
parasme sanjay <parasme.sanjay@gmail.com>
2017-09-28 22:50:08 +0530

    abs

--- AllCool.pl/ViewController/Festival/FestivalListVC.m
+++ AllCool.pl/ViewController/Festival/FestivalListVC.m
@@ -16,6 +16,8 @@
     IBOutlet UIView *viewUpButtons;
     
     int tag;
+    
+    NSArray *arrFest, *arrNewFest, *arrOldFest;
 }
 @end
 
@@ -31,6 +33,66 @@
     viewUpButtons.layer.borderWidth = 1;
     
     tag =1;
+}
+
+-(void) get_New_Festival
+{
+    // http://allcool.pl/api_ios/festival/new_in_festival.php
+    
+    SVHUD_START
+    [WebServiceCalls GET:@"festival/new_in_festival.php" parameter:nil completionBlock:^(id JSON, WebServiceResult result)
+     {
+         SVHUD_STOP
+         NSLog(@"%@", JSON);
+         
+         @try
+         {
+             if ([JSON[@"success"] integerValue] == 1)
+             {
+                 arrNewFest = JSON[@"festivals"];
+             }
+             else
+             {
+                 [WebServiceCalls alert:@"Unable to fetch data. try again"];
+             }
+         }
+         @catch (NSException *exception)
+         {
+         }
+         @finally
+         {
+         }
+     }];
+}
+
+-(void) get_Old_Festival
+{
+    // http://allcool.pl/api_ios/festival/out_festival.php
+    
+    SVHUD_START
+    [WebServiceCalls GET:@"festival/out_festival.php" parameter:nil completionBlock:^(id JSON, WebServiceResult result)
+     {
+         SVHUD_STOP
+         NSLog(@"%@", JSON);
+         
+         @try
+         {
+             if ([JSON[@"success"] integerValue] == 1)
+             {
+                 arrNewFest = JSON[@"festivals"];
+             }
+             else
+             {
+                 [WebServiceCalls alert:@"Unable to fetch data. try again"];
+             }
+         }
+         @catch (NSException *exception)
+         {
+         }
+         @finally
+         {
+         }
+     }];
 }
 
 - (void)didReceiveMemoryWarning
